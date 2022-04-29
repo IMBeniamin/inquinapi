b9606e0fe130c648f80430f7f21b5214cec1e13c
Accardo <balzacca.raffaele@gmail.com>
2022-04-29 11:44:36 +0200

    Changes by siorga

--- routes/API.js
+++ routes/API.js
@@ -5,6 +5,77 @@ const Api = require("../model/countryScheme");
 
 router.options("*", cors());
 
+const parse_isoCode = (raw_isoCode, error) => {
+    const iso_code_format = new RegExp("^\\w{3}(?:,\\w{3})*$");
+    if (raw_isoCode === undefined || !raw_isoCode)
+        return undefined
+
+    // remove spaces and formats according to model then test against regex pattern
+    else if (iso_code_format.test(raw_isoCode.replace(/\s/g, "").toUpperCase()))
+        return { $in: raw_isoCode.split(",") }
+
+    else
+    {
+        error["iso_code"] = {
+            message: "iso_code does not respect the {AAA,AAA,...} format",
+        };
+        return undefined
+    }
+}
+
+const parse_year = (raw_year, error) => {
+    const year_format_list = new RegExp("^-?\\d+(?:,-?\\d+)*$");
+    const year_format_range = new RegExp("^(-?\\d+)-(-?\\d+)$");
+
+    if (raw_year === undefined || !raw_year)
+        return undefined;
+
+    else if (year_format_list.test(raw_year))
+        return { $in: raw_year.split(",") };
+
+    else if (year_format_range.test(raw_year))
+    {
+        const range = year_format_range.exec(raw_year).slice(1);
+        return { $gt: Math.min(...range), $lt: Math.max(...range) }
+    }
+
+    else
+    {
+        error["year"] = {
+            message:
+                "year does not respect the {YYYY,YYYY,...} or {YYYY-YYYY} format",
+        };
+        return undefined
+    }
+}
+
+const parse_filter = (raw_filter, error) => {
+    // checks filter formatting {[-]field1,[-]field2,...}
+    const filter_format = new RegExp("^(-?)\\w+(?:(,?)\\1\\w+)*$");
+    const default_filter = ['-_id']
+    let filter = raw_filter
+        ? raw_filter.replace(/\s/g, "")
+        : undefined;
+
+    if (filter === undefined || !filter)
+        return default_filter;
+
+    else if (filter_format.test(filter))
+        return ['-_id'].concat(filter.split(","))
+
+    else
+    {
+        error["filter"] = {
+            message: "filter does not respect the {[-]field1,[-]field2,...} format",
+        };
+        return default_filter
+    }
+}
+
+const parse_strict = (raw_strict, error) => {
+    return false
+}
+
 router.get("/", (req, res) => {
     let error = {};
 
@@ -29,6 +100,7 @@ router.get("/", (req, res) => {
         else
             res.status(200).json(db_data);
     });
+
 });
 
 module.exports = router;
